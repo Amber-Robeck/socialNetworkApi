a1ce5facc143cf956fa73892b8d6774f4d775ab6
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-02-14 18:36:42 -0600

    added thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,33 @@
+const { Schema, model } = require('mongoose');
 
+// Schema to create user model
+const thoughtSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        thoughtText: {
+            type: String,
+            required: true,
+            unique: true,
+            max_length: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        reactions: [reactionSchema],
+
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
