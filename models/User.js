0592db87616f4e74a7e47cf76e0d668e0e2d5050
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-02-14 18:25:27 -0600

    started User schema, added connection to connection.js, server.js also created pkg JSON

--- models/User.js
+++ models/User.js
@@ -1,1 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create user model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true
+            //trimmed?
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+            //email validation
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
 
